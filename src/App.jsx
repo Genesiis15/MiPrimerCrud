501e55fb4f84a7ec379da36bf25f974d11805ce4
manguila2525 <manguilas2525@gmail.com>
2023-09-08 09:36:54 -0400

    Bg white

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-
-import { useState } from "react";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App () {
-
-    return (
-        <section className="App">
-     <TwitterFollowCard    userName="midudev" > 
-        <h1>Hola</h1>
-        Miguel Angel Duran 
-     </TwitterFollowCard>
-
-     
-       
-     <TwitterFollowCard  initialIsFollowing userName="pheralb" > 
-
-        Pablo Hernandez  
-    </TwitterFollowCard>
-
-       
-
-   
-        </section>
-    )
+    return <>
+<TwitterFollowCard userName={'midudev'} name={"Genesis Rojas"} isfollowing={'Seguir'}/>
+     <TwitterFollowCard userName={'pheralb'} name={"Mariangel Rojas"} isfollowing={'Seguir'}/>
+     <TwitterFollowCard userName={'elonmuske '} name={"Miguel Angel Duran"} isfollowing={'Seguir'}/>
+    </>
     
 }
